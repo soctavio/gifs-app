59a680bb5e48bd47490cfb890384e2183926658f
Sergio Octavio <sergio.sanmartinf@gmail.com>
2020-07-30 17:44:53 -0400

    Docs directory added

---
+++ docs/precache-manifest.71c5f74bef7b4fa84d16f5bbd6b5823f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316942ed6378083bc99538ec69d7f8e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32bfc5b8c71074a7ea1d",
+    "url": "/static/css/main.c0b01ae4.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32bfc5b8c71074a7ea1d",
+    "url": "/static/js/main.0ade5175.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
